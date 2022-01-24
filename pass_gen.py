85e116625527b508fe06af2fbaf40c70a88ca9fb
Boyd G <contact@webcrack.tech>
2022-01-23 17:50:14 -0800

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -9,9 +9,9 @@ x = input('PW Mnemonic id: ')
 path = './.later_retrieval.log' # Plan text log in home directory, set as a hidden dot file.
 later = open(path, 'a')
 
-ok_punc = '(#!_~)' # Custom list of punctuation 
+ok_punc = '(#!_~)' # Custom list of punctuation.
 characters = string.ascii_letters + string.digits + ok_punc
-p = "".join(choice(characters)for x in range(randint(16, 16))) # Number of charactors long
+p = "".join(choice(characters)for x in range(randint(16, 16))) # Number of charactors long.
 
 print(p)
 later.write('\n' + x + ': ' + (p))
